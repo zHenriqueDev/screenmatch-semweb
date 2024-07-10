3ecc8f5257be0d55e304f041f1097783e215c812
zHenriqueDev <henriquew1533@hotmail.com>
2024-07-10 15:16:39 -0300

    news

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,11 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-			var consumoApi = new ConsumoApi();
-			var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-			System.out.println(json);
-			ConverteDados conversor = new ConverteDados();
-			DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-			System.out.printf(String.valueOf(dados));
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
